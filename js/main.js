2f8122e4fb9dbe2c714b4add42d7388dde502086
disoownedd <manumastrotta@gmail.com>
2023-03-11 01:14:57 -0300

    Se finalizo main.js V 1.0

--- js/main.js
+++ js/main.js
@@ -5,18 +5,16 @@ const btn = document.querySelector('#btn')
 
 const color = document.querySelector('.color')
 
-const getRandom03=() =>{
-    let random = Math.random()
-    random = random * 3
-    random = parseInt(random)
-    return random
-}
-
 /* Creamos el detector de ventos de clicl: */
 btn.addEventListener('click', function(){
     // console.log(document.body)
     // Obtener un numero aleatorio entre 0 - 3 /* Entre 0 y 3 para poder moverse por las matrices de la constante "colors" */
-    const randomNumber= 2
+    const randomNumber= getRandomNumber()
+    console.log(randomNumber)
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
+/* Math.floor provocara que siempre se redondee hacia abajo */
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
